3611cacfcacc1bc9ce10b7b5640738b395c696d8
LeoSallu <L.sallustio99@gmail.com>
2023-02-22 22:17:05 +0100

    tentativi di fix falliti calcolo non funzionante

--- js/main.js
+++ js/main.js
@@ -1,26 +1,28 @@
 'use strict';
-    //EVENTO
+
+
+const prezzoPerKm = 0.21;
+
+const kmPercorsi = document.getElementById("km_percorsi").value;
+const etaUtente = document.getElementById("etaUtente");
+const prezzoTot = kmPercorsi * prezzoPerKm; 
+let prezzoMinorenni = prezzoTot - (prezzoTot * 0.2);
+let prezzoOver = prezzoTot - (prezzoTot * 0.4);
+
+//EVENTO
     document.getElementById('genera').addEventListener('click', 
     function(){
         //-RICHIESTA KM 
-            const prezzoPerKm = 0.21;
-            const kmPercorsi = document.getElementById("km_percorsi").value;
-            let prezzoTot = ((kmPercorsi * prezzoPerKm).toFixed(2)); 
-            console.log(prezzoTot+' €'); 
-            document.getElementById('test').innerHTML = (prezzoTot +' €');
         //-RICHIESTA ETÁ
-        let età = document.getElementById("età").value;        
-    if(età="Minorenne"){
-        let prezzoScontato = ((prezzoTot - (prezzoTot * 0.2)).toFixed(2));
-        console.log(prezzoScontato + ' €');
-        document.getElementById('test').innerHTML = (prezzoScontato +' €');
-    } else if(età="Over60"){
-        let prezzoScontato = prezzoTot - ((prezzoTot * 0.4).toFixed(2));
-        console.log(prezzoScontato + ' €');
-        document.getElementById('test').innerHTML = (prezzoScontato +' €');
+        if(etaUtente='minorenne'){
+            console.log(prezzoMinorenni + ' €');
+            document.getElementById('test').innerHTML = (prezzoMinorenni +' €');
+        } else if(etaUtente='over60'){
+            console.log(prezzoOver + ' €');
+            document.getElementById('test').innerHTML = (prezzoOver +' €');
 
-    } else{
-        console.log(prezzoTot + ' €');
-        document.getElementById('test').innerHTML = (prezzoTot +' €');
-    }
+        } else{
+            console.log(prezzoTot + ' €');
+            document.getElementById('test').innerHTML = (prezzoTot +' €');
+        }
     })   
