36448019d5962e0c6f9224329f3c1cd3bae7b46a
LeoSallu <L.sallustio99@gmail.com>
2023-02-22 19:07:23 +0100

    problemi calcolo età(sconto a tutte option con if)

--- js/main.js
+++ js/main.js
@@ -1,12 +1,26 @@
 'use strict';
+    //EVENTO
+    document.getElementById('genera').addEventListener('click', 
+    function(){
+        //-RICHIESTA KM 
+            const prezzoPerKm = 0.21;
+            const kmPercorsi = document.getElementById("km_percorsi").value;
+            let prezzoTot = ((kmPercorsi * prezzoPerKm).toFixed(2)); 
+            console.log(prezzoTot+' €'); 
+            document.getElementById('test').innerHTML = (prezzoTot +' €');
+        //-RICHIESTA ETÁ
+        let età = document.getElementById("età").value;        
+    if(età="Minorenne"){
+        let prezzoScontato = ((prezzoTot - (prezzoTot * 0.2)).toFixed(2));
+        console.log(prezzoScontato + ' €');
+        document.getElementById('test').innerHTML = (prezzoScontato +' €');
+    } else if(età="Over60"){
+        let prezzoScontato = prezzoTot - ((prezzoTot * 0.4).toFixed(2));
+        console.log(prezzoScontato + ' €');
+        document.getElementById('test').innerHTML = (prezzoScontato +' €');
 
-//RICHIESTA KM 
-
-const prezzoPerKm = 0.21;
-const kmPercorsi = document.getElementById("km_percorsi").value;
-let prezzoTot = kmPercorsi * prezzoPerKm;
-
-console.log(prezzoTot+' €');
-
-document.getElementById('test').innerHTML = prezzoTot;
-
+    } else{
+        console.log(prezzoTot + ' €');
+        document.getElementById('test').innerHTML = (prezzoTot +' €');
+    }
+    })   
