9af369294e9cb8c77121c397b9df558ab49e99fa
LeoSallu <L.sallustio99@gmail.com>
2023-02-23 12:12:54 +0100

    fix vari ma problemi con sconto over non risulta

--- js/main.js
+++ js/main.js
@@ -3,6 +3,15 @@
 const nomeUtente = document.getElementById('nomeUtente');
 const kmPercorsi = document.getElementById('km_percorsi');
 const etaUtente = document.getElementById('etaUtente');
+
+//VARIABILI STAMPA HTML
+const testoNome = document.getElementById('nome');
+const testoOfferta = document.getElementById('offerta');
+const testoCarrozza = document.getElementById('carrozza');
+const testoCp = document.getElementById('cp');
+const prezzo = document.getElementById('prezzo'); 
+const risultatoBiglietto = document.querySelector('biglietto_treno');         
+
 //EVENTO
     document.getElementById('genera').addEventListener('click', 
     function(){
@@ -13,19 +22,26 @@ const etaUtente = document.getElementById('etaUtente');
         console.log({inputNomeUtenteValue, inputKmValue, inputEtaValue});
         // PREZZO E IF
         let prezzoTot = 0.21 * inputKmValue;
-        
-        if(inputEtaValue==='minorenne'){
+        let messaggio = 'Biglietto Standard';
+        if(inputEtaValue==='Minorenne'){
+            messaggio = 'Sconto minorenne';
             prezzoTot = prezzoTot * 0.8;
         }
-        else if(inputEtaValue==='over60'){
+        else if(inputEtaValue==='Over'){
+            messaggio = 'Sconto over';
             prezzoTot = prezzoTot * 0.6;
         }
 
         prezzoTot = prezzoTot.toFixed(2);
         console.log(prezzoTot + ' €'); 
+       
         //STAMPA NEL HTML
-        document.getElementById('test-name').innerHTML = inputNomeUtenteValue;
-        document.getElementById('test-km').innerHTML = inputKmValue;   
-        document.getElementById('test-age').innerHTML = inputEtaValue;  
-        document.getElementById('test-price').innerHTML = prezzoTot + ' €';          
+        testoNome.innerHTML = inputNomeUtenteValue;
+        testoOfferta.innerHTML = messaggio;
+        //testoCarrozza.innerHTML =;
+        //testoCp.innerHTML =;
+        prezzo.innerHTML = prezzoTot + ' €';
+
+        // TOOGLE
+        // risultatoBiglietto.classList.add('show');
      })   
