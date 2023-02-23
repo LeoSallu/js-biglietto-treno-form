3d7ef6351b04a6d8355df4be725c610c6415fd4a
LeoSallu <L.sallustio99@gmail.com>
2023-02-23 11:10:01 +0100

    fix al codice

--- js/main.js
+++ js/main.js
@@ -1,28 +1,31 @@
 'use strict';
-
-
-const prezzoPerKm = 0.21;
-
-const kmPercorsi = document.getElementById("km_percorsi").value;
-const etaUtente = document.getElementById("etaUtente");
-const prezzoTot = kmPercorsi * prezzoPerKm; 
-let prezzoMinorenni = prezzoTot - (prezzoTot * 0.2);
-let prezzoOver = prezzoTot - (prezzoTot * 0.4);
-
+//IMPOSTAZIONI VARIABILI
+const nomeUtente = document.getElementById('nomeUtente');
+const kmPercorsi = document.getElementById('km_percorsi');
+const etaUtente = document.getElementById('etaUtente');
 //EVENTO
     document.getElementById('genera').addEventListener('click', 
     function(){
-        //-RICHIESTA KM 
-        //-RICHIESTA ETÁ
-        if(etaUtente='minorenne'){
-            console.log(prezzoMinorenni + ' €');
-            document.getElementById('test').innerHTML = (prezzoMinorenni +' €');
-        } else if(etaUtente='over60'){
-            console.log(prezzoOver + ' €');
-            document.getElementById('test').innerHTML = (prezzoOver +' €');
-
-        } else{
-            console.log(prezzoTot + ' €');
-            document.getElementById('test').innerHTML = (prezzoTot +' €');
+        //IMPOSTAZIONE COSTANTI INPUT DEL FORM
+        const inputNomeUtenteValue = nomeUtente.value;
+        const inputKmValue = Number(kmPercorsi.value);
+        const inputEtaValue = etaUtente.value;
+        console.log({inputNomeUtenteValue, inputKmValue, inputEtaValue});
+        // PREZZO E IF
+        let prezzoTot = 0.21 * inputKmValue;
+        
+        if(inputEtaValue==='minorenne'){
+            prezzoTot = prezzoTot * 0.8;
         }
-    })   
+        else if(inputEtaValue==='over60'){
+            prezzoTot = prezzoTot * 0.6;
+        }
+
+        prezzoTot = prezzoTot.toFixed(2);
+        console.log(prezzoTot + ' €'); 
+        //STAMPA NEL HTML
+        document.getElementById('test-name').innerHTML = inputNomeUtenteValue;
+        document.getElementById('test-km').innerHTML = inputKmValue;   
+        document.getElementById('test-age').innerHTML = inputEtaValue;  
+        document.getElementById('test-price').innerHTML = prezzoTot + ' €';          
+     })   
